33b07f38e8cc1f736462486f51e4f6496f540e55
Stephen Tan <stephen2502@hotmail.co.uk>
2019-12-03 15:19:15 +1100

    Figured Out Buttons

---
+++ RestaurantTycoon/Assets/Scriptable Objects/Base.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+    public string itemName;
+
+    public int numberOfPrefabsToCreate;
+    public Vector3[] spawnPoints;
+}
