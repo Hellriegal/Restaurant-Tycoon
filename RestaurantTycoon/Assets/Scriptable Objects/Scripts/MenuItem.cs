2c39610ebbbd80d0ce2df65419d8ec380115d5a2
Stephen Tan <stephen2502@hotmail.co.uk>
2019-12-05 19:31:38 +1100

    Unity Update
    Also created basic Menu Item script.

---
+++ RestaurantTycoon/Assets/Scriptable Objects/Scripts/MenuItem.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Menu Item", menuName = "Scriptable Objects/Menu Item", order = 1)]
+public class MenuItem : ScriptableObject
+{
+    public string ItemName;
+    public Sprite image;
+}
