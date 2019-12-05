a01742e6d6e3071e4102b88de405d1b6d3ef3e3b
Stephen Tan <stephen2502@hotmail.co.uk>
2019-12-05 15:21:04 +1100

    Buying Ingredients
    Established the system to buy ingredients with 1 script and 3 ScriptableObjects. Currently only potatoes have been added.

---
+++ RestaurantTycoon/Assets/Scriptable Objects/Scripts/IngredientPrices.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingredient Prices", menuName = "Scriptable Objects/Ingredient Prices", order = 1)]
+public class IngredientPrices : ScriptableObject
+{
+    public int Basil;
+    public int Bread;
+    public int Breadcrumbs;
+    public int Butter;
+    public int Cabanossi;
+    public int Cheese;
+    public int Chicken;
+    public int CoffeeBeans;
+    public int Corn;
+    public int Custard;
+    public int Dough;
+    public int Garlic;
+    public int Ham;
+    public int IceCream;
+    public int Lemonade;
+    public int Peas;
+    public int Pepperoni;
+    public int Pineapple;
+    public int Potato;
+    public int Pumpkin;
+    public int Steak;
+    public int Sugar;
+    public int TeaLeaves;
+    public float Tomato;
+    public int TomatoBase;
+
+    public void pricePass()
+    {
+        
+    }
+}
+
---
+++ RestaurantTycoon/Assets/Scriptable Objects/Scripts/PlayerStats.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Stats", menuName = "Scriptable Objects/Player Stats", order = 1)]
+public class PlayerStats : ScriptableObject
+{
+   public float Money;
+}
