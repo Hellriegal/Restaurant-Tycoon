867da0219a70488c6b5902f27bbc7ef571ddfb08
Stephen Tan <stephen2502@hotmail.co.uk>
2020-01-17 19:21:48 +1100

    PathFinding Work
    Currently not working because it doesn't always detect the tile types

--- RestaurantTycoon/Assets/Scripts/Decorating/tileLocate.cs
+++ RestaurantTycoon/Assets/Scripts/Decorating/tileLocate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System;
+using System.Linq;
 
 public class tileLocate : MonoBehaviour
 {
@@ -17,10 +18,31 @@ public class tileLocate : MonoBehaviour
             position = pos;
         }
     }
+    [Serializable]
+    public struct TileCandidate
+    {
+        public Vector3Int position;
+        public float cost;
+        public TileCandidate(Vector3Int pos, float dis)
+        {
+            position = pos;
+            cost = dis;
+        }
+    }
 
     Tilemap tilemap;
     GridLayout gridLayout;
+
     public List<TileInfo> tiles;
+    public List<TileCandidate> nextPositions;
+    public List<float> nextPosCost;
+
+    public TileBase obstacle;
+    
+    Vector3Int start = new Vector3Int(0,0,0);
+    [SerializeField]
+    Vector3Int goal = new Vector3Int(10,15,0);
+    Vector3Int currentPos = new Vector3Int(0,0,0);
     
     // Start is called before the first frame update
     void Start()
@@ -29,12 +51,16 @@ public class tileLocate : MonoBehaviour
         gridLayout = transform.parent.GetComponentInParent<GridLayout>();
         getAllTilePositions();
         TileInfo temp = new TileInfo(tilemap.GetTile(new Vector3Int(1,1,0)), new Vector3Int(1,1,0));
+        Debug.DrawLine(gridLayout.CellToWorld(start), gridLayout.CellToWorld(goal), Color.blue, Mathf.Infinity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentPos != goal)
+        {
+            pathFind();
+        }
     }
 
     public void getAllTilePositions()
@@ -49,6 +75,45 @@ public class tileLocate : MonoBehaviour
                 tiles.Add (new TileInfo (tilemap.GetTile(new Vector3Int(i, j, 0)), new Vector3Int(i, j, 0)));
             }
         }
-        Debug.Log(tiles[1].tileType + "pos" + tiles[1].position);
+    }
+
+    public float distanceFromPoint(Vector3Int start, Vector3Int goal)
+    {
+        float distance = 0;
+        distance = Mathf.Sqrt(Mathf.Pow(Mathf.Abs(start.x - goal.x), 2) + Mathf.Pow(Mathf.Abs(start.y - goal.y), 2));
+        return distance;
+    }
+
+    public void pathFind()
+    {
+        Vector3Int initStart = currentPos;
+        nextPositions.Clear();
+
+        nextPositions.Add(new TileCandidate (new Vector3Int (currentPos.x -1, currentPos.y, 0), ((distanceFromPoint(new Vector3Int (currentPos.x -1, currentPos.y, 0), goal))) ));
+        nextPositions.Add(new TileCandidate (new Vector3Int (currentPos.x +1, currentPos.y, 0), ((distanceFromPoint(new Vector3Int (currentPos.x +1, currentPos.y, 0), goal))) ));
+        nextPositions.Add(new TileCandidate (new Vector3Int (currentPos.x, currentPos.y -1, 0), ((distanceFromPoint(new Vector3Int (currentPos.x, currentPos.y -1, 0), goal))) ));
+        nextPositions.Add(new TileCandidate (new Vector3Int (currentPos.x, currentPos.y +1, 0), ((distanceFromPoint(new Vector3Int (currentPos.x, currentPos.y +1, 0), goal))) ));
+        
+        for (int i = 0; i < 4; i++)
+        {
+            if (tilemap.GetTile(nextPositions[i].position) != obstacle)
+            {
+                nextPosCost.Add(nextPositions[i].cost);
+            }
+            else if (tilemap.GetTile(nextPositions[i].position) != obstacle)
+            {
+                Debug.Log("Hit");
+            }
+        }
+
+        float lowestCost = nextPosCost.Min();
+        for (int i = 0; i < nextPositions.Count; i++)
+        {
+            if (nextPositions[i].cost == lowestCost)
+            {
+                currentPos = nextPositions[i].position;
+            }
+        }
+        Debug.DrawLine(gridLayout.CellToWorld(initStart), gridLayout.CellToWorld(currentPos), Color.red, Mathf.Infinity);
     }
 }
