1488f9116264b6ad47a521240719d653ebc9bba2
Stephen Tan <stephen2502@hotmail.co.uk>
2020-01-17 15:21:47 +1100

    Created Script that will retrieve all tiles in a tilemap
    The script will add a data type containing the TileBase and the tile position in tilemap coordinates,

---
+++ RestaurantTycoon/Assets/Scripts/Decorating/tileLocate.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+
+public class tileLocate : MonoBehaviour
+{
+    [Serializable]
+    public struct TileInfo
+    {
+        public TileBase tileType;
+        public Vector3Int position;
+        public TileInfo(TileBase tile, Vector3Int pos)
+        {
+            tileType = tile;
+            position = pos;
+        }
+    }
+
+    Tilemap tilemap;
+    GridLayout gridLayout;
+    public List<TileInfo> tiles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+        getAllTilePositions();
+        TileInfo temp = new TileInfo(tilemap.GetTile(new Vector3Int(1,1,0)), new Vector3Int(1,1,0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getAllTilePositions()
+    {
+        Vector3Int startingPosition = new Vector3Int(tilemap.cellBounds.position.x, tilemap.cellBounds.position.y, tilemap.cellBounds.position.z);
+        Vector3Int endPosition = new Vector3Int(tilemap.cellBounds.position.x + (tilemap.cellBounds.size.x -1), tilemap.cellBounds.position.y + (tilemap.cellBounds.size.y -1), tilemap.cellBounds.position.z + (tilemap.cellBounds.size.z -1));
+
+        for (int i = startingPosition.x; i <= endPosition.x; i++)
+        {
+            for (int j = startingPosition.y; j <= endPosition.y; j++)
+            {
+                tiles.Add (new TileInfo (tilemap.GetTile(new Vector3Int(i, j, 0)), new Vector3Int(i, j, 0)));
+            }
+        }
+        Debug.Log(tiles[1].tileType + "pos" + tiles[1].position);
+    }
+}
